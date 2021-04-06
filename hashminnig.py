8701154b2770ca764ca795c945933d98e8eca1bc
mmuckensturm <mmuckensturm-presta@cora.fr>
2021-04-06 10:43:38 +0200

    [Info] added additional comments

--- hashminnig.py
+++ hashminnig.py
@@ -7,19 +7,20 @@ from hashlib import sha256
 import time
 
 # settings
-MAX_NONCE = 100000000000
+MAX_NONCE = 1000000000
 
 # reading information as unicode string from the file
 def SHA256(text):
     """
+    Takes in a string text, returns the string of sha256
     * text:-> string that will be hash
     """
-
     return sha256(text.encode("ascii")).hexdigest()
 
 # constructing new hash key and finding nonce number
 def mine(block_number, transactions, previous_hash, prefix_zeros):
     """
+    Takes in multiple arguments listed below, returns the desired new hash
     * block_number:-> int of the block number
     * transactions:-> string that contains all transactions of this block number
     * previous_hash:-> previous hash encoding
@@ -36,7 +37,7 @@ def mine(block_number, transactions, previous_hash, prefix_zeros):
             print(f"Successfully mined bitcoins with nonce value : {nonce}")
             return new_hash
 
-    raise BaseException(f"Couldn't find correct has after trying {MAX_NONCE} times")
+    raise BaseException(f"Couldn't find correct hash after trying {MAX_NONCE} times")
 
 
 if __name__ == "__main__":
@@ -44,7 +45,8 @@ if __name__ == "__main__":
     # settings
     transactions = "Marc/Jules->20, Mike/Cara->45 Toto/Momo->100"
     difficulty = 5  # higher number will take more time for mining as difficulty increases (20 for bitcoin mining)
-    number_blocks = 5 # number of the transaction's block 
+    number_blocks = 5 # number of the transaction's block
+    previous_hash = "0000000xa036944e29568d0cff17edbe038f81208fecf9a66be9a2b8321c6ec7" # previous sha256 key started with 7 zero
 
     # initialized counter
     start = time.time()
@@ -52,7 +54,7 @@ if __name__ == "__main__":
     new_hash = mine(
         number_blocks,
         transactions,
-        "0000000xa036944e29568d0cff17edbe038f81208fecf9a66be9a2b8321c6ec7",
+        previous_hash,
         difficulty,
     )
     total_time = str((time.time() - start))
